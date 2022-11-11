2e39513383f9e25150c3abf4bb84b2fe4047681a
Eva Reitzel <evareitzel@gmail.com>
2022-11-11 14:25:20 -0500

    Complete!

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require 'pry'
+ 
+ 
+def reverse_each_word str
+  str.split.map(&:reverse).join(" ")
+end
+ 
+ 
+ 
+ 
+def reverse_each_word_v1 str
+  arr = str.split
+  new_arr = []
+ 
+  arr.each do |el|
+    new_arr << el.reverse
+  end
+ 
+  new_arr.join(" ")
+end
